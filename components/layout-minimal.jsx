e9aa6b644b0540d5524bfe9c47868a0ad85ec9da
Meshach Mitchell <meshach.mitchell@gmail.com>
2022-03-15 14:48:53 -0500

    Initial setup for heroku deployment

---
+++ components/layout-minimal.jsx
@@ -0,0 +1,9 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+export default function MinimalLayout({ children }) {
+  return <div className="layout">
+    {children}
+  </div>
+}
